3e23eb8516f3c62640956595b0ccdd5edc4bd006
giacomo-spadoni <giaky.spado@gmail.com>
2024-04-23 19:13:15 +0200

    M2-complited M3-button created

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 const slides = document.getElementsByClassName('slide')
 
 for (let i = 0; i < slides.length; i++) {
-    if (slides[i].classList.contains('active')) {
+    if (slides[i].classList.contains('active') && i != slides.length - 1) {
         slides[i].nextElementSibling.classList.add('active')
+        slides[i].classList.remove('active')
+        break
+    } else if (slides[i].classList.contains('active') && i == slides.length - 1) {
+        slides[0].classList.add('active')
         slides[i].classList.remove('active')
         break
     }
