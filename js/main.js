6234909cbf5176e02f3a582c2fd8080d8c1e38df
giacomo-spadoni <giaky.spado@gmail.com>
2024-04-23 19:19:24 +0200

    M2 and M3 and first bonus complited

--- js/main.js
+++ js/main.js
@@ -1,14 +1,31 @@
 const slides = document.getElementsByClassName('slide')
+const upButton = document.getElementById('bottone-su')
+const downButton = document.getElementById('bottone-giu')
 
-for (let i = 0; i < slides.length; i++) {
-    if (slides[i].classList.contains('active') && i != slides.length - 1) {
-        slides[i].nextElementSibling.classList.add('active')
-        slides[i].classList.remove('active')
-        break
-    } else if (slides[i].classList.contains('active') && i == slides.length - 1) {
-        slides[0].classList.add('active')
-        slides[i].classList.remove('active')
-        break
+downButton.addEventListener('click', function () {
+    for (let i = 0; i < slides.length; i++) {
+        if (slides[i].classList.contains('active') && i != slides.length - 1) {
+            slides[i].nextElementSibling.classList.add('active')
+            slides[i].classList.remove('active')
+            break
+        } else if (slides[i].classList.contains('active') && i == slides.length - 1) {
+            slides[0].classList.add('active')
+            slides[i].classList.remove('active')
+            break
+        }
     }
-}
+})
 
+upButton.addEventListener('click', function () {
+    for (let i = 0; i < slides.length; i++) {
+        if (slides[i].classList.contains('active') && i != 0) {
+            slides[i].previousElementSibling.classList.add('active')
+            slides[i].classList.remove('active')
+            break
+        } else if (slides[i].classList.contains('active') && i == 0) {
+            slides[slides.length - 1].classList.add('active')
+            slides[i].classList.remove('active')
+            break
+        }
+    }
+})
